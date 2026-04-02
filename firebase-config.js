cde0851b8d7a380611c0dbf12874334ea18b446f
Omran AlBakkar <88229546+omranalbakkar@users.noreply.github.com>
2026-04-02 15:20:08 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,38 +1,46 @@
 // firebase-config.js
 
-// 1. Import necessary Firebase modules (v10.8.0)
+// 1. Import ALL necessary Firebase modules (v10.8.0)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+
+// Auth
 import { 
-    getAuth, 
-    onAuthStateChanged, 
-    signInWithEmailAndPassword, 
-    signOut, 
-    createUserWithEmailAndPassword, 
-    updatePassword, 
-    reauthenticateWithCredential, 
-    EmailAuthProvider 
+  getAuth, 
+  onAuthStateChanged, 
+  signInWithEmailAndPassword, 
+  signOut, 
+  createUserWithEmailAndPassword, 
+  updatePassword, 
+  reauthenticateWithCredential, 
+  EmailAuthProvider 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+// Firestore (Added missing getCountFromServer, addDoc, etc.)
 import { 
-    getFirestore, 
-    doc, setDoc, getDoc, updateDoc, deleteDoc, 
-    collection, query, where, getDocs, 
-    serverTimestamp, orderBy, limit 
+  getFirestore, 
+  doc, setDoc, getDoc, updateDoc, deleteDoc, 
+  collection, query, where, getDocs, 
+  serverTimestamp, orderBy, limit,
+  addDoc, // <-- Added
+  getCountFromServer // <-- Added
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// Realtime Database
 import { 
-    getDatabase, 
-    ref, set, push, onValue, update, remove, child, get 
+  getDatabase, 
+  ref, set, push, onValue, update, remove, child, get 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 // 2. Your NEW Web App Configuration (EduBoard Project)
 const firebaseConfig = {
-    apiKey: "AIzaSyCDmP9SERcSOmf7G0g70g8UVNB44H0zw_8",
-    authDomain: "eduboard-870d3.firebaseapp.com",
-    databaseURL: "https://eduboard-870d3-default-rtdb.firebaseio.com",
-    projectId: "eduboard-870d3",
-    storageBucket: "eduboard-870d3.firebasestorage.app",
-    messagingSenderId: "658705588696",
-    appId: "1:658705588696:web:9e55ce87364f26a9fb52ad",
-    measurementId: "G-LMDZHSMSB7"
+  apiKey: "AIzaSyCDmP9SERcSOmf7G0g70g8UVNB44H0zw_8",
+  authDomain: "eduboard-870d3.firebaseapp.com",
+  databaseURL: "https://eduboard-870d3-default-rtdb.firebaseio.com",
+  projectId: "eduboard-870d3",
+  storageBucket: "eduboard-870d3.firebasestorage.app",
+  messagingSenderId: "658705588696",
+  appId: "1:658705588696:web:9e55ce87364f26a9fb52ad",
+  measurementId: "G-LMDZHSMSB7"
 };
 
 // 3. Initialize Firebase
@@ -43,19 +51,19 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const rtDB = getDatabase(app);
 
-// 5. Export functions for use in HTML pages
+// 5. Export ALL functions for use in HTML pages
 export { 
-    onAuthStateChanged, 
-    signInWithEmailAndPassword, 
-    signOut, 
-    createUserWithEmailAndPassword, 
-    updatePassword, 
-    reauthenticateWithCredential, 
-    EmailAuthProvider,
-    doc, setDoc, getDoc, updateDoc, deleteDoc, 
-    collection, query, where, getDocs, 
-    serverTimestamp, orderBy, limit,
-    ref, set, push, onValue, update, remove, child, get
+  onAuthStateChanged, 
+  signInWithEmailAndPassword, 
+  signOut, 
+  createUserWithEmailAndPassword, 
+  updatePassword, 
+  reauthenticateWithCredential, 
+  EmailAuthProvider,
+  doc, setDoc, getDoc, updateDoc, deleteDoc, addDoc, // <-- Added addDoc
+  collection, query, where, getDocs, getCountFromServer, // <-- Added getCountFromServer
+  serverTimestamp, orderBy, limit,
+  ref, set, push, onValue, update, remove, child, get
 };
 
 console.log("✅ EduBoard Connected to: eduboard-870d3");
